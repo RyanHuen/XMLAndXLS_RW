d8e727bc9f23151d583a5a0fbe01d3268b57eefc
xuanweihong <xuanweihong@phoenixos.com>
2016-11-02 16:26:01 +0800

    添加新的xml读取写入为xls的方式

---
+++ src/ryan/xml_to_xls/ReadStringAndWrite.java
@@ -0,0 +1,30 @@
+package ryan.xml_to_xls;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Map;
+
+import ryan.read.ReadFromXML;
+import ryan.write.WriteTOExcel;
+
+/**
+ * Created by ryanhuencompany on 16-11-2.
+ */
+public class ReadStringAndWrite {
+    /**
+     * 需要读取的中文字符串文件strings.xml
+     */
+    public static final String STRING_FILE_PATH_ZH_CN = "/home/ryanhuencompany/Documents/values-zh-rCN/strings.xml";
+    /**
+     * 要把数据写入的excel文件所在的路径，不包括excel文件名称
+     */
+    public static final String OUTPUT_EXCEL_FILE_PATH = "/home/ryanhuencompany/Documents/test.xls";
+
+    public static void main(String[] args) {
+        ReadFromXML readFromXML = new ReadFromXML(new File(STRING_FILE_PATH_ZH_CN));
+        Map<String, String> map = readFromXML.getMaps();
+        WriteTOExcel.writeToExcelDouble(OUTPUT_EXCEL_FILE_PATH, new ArrayList<String>(map.keySet()), new ArrayList<String>(map.values()), 0);
+
+
+    }
+}
